f9677fca3898af91b8d21a70541a9d4dc7e766d5
nshuti_hirwa_landry <hirwanshuti40@gmail.com>
2023-07-15 17:27:04 +0200

    init small tailwindcss project

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  darkMode: "class",
+  content: ["./src/**/*.{html,js}"],
+  theme: {},
+  plugins: [require("./plugins/openVariant")],
+};
